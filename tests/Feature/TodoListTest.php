b0afa5c3e1cd061dc79f4412c365b30897ebeabc
FarhadMusayev <ferhad.musayev.00@gmail.com>
2022-10-05 01:24:13 +0400

    test #2 for todo-list

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,14 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
-    public function test_index_todo_list()
+    use RefreshDatabase;
+
+    public function test_get_todo_list()
     {
+        TodoList::create(['name' => 'Farhad']);
+
         $response = $this->getJson(route('todo-list.index'));
 
         $this->assertEquals(1, count($response->json()));
